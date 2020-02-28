50f57ae2f4ccefdddcf299292c0488c26c66d8bd
Christopher D. Long <galizur@gmail.com>
2020-02-28 03:42:05 -0500

    Basic mortality

---
+++ analytics/mortality.sql
@@ -0,0 +1,11 @@
+select
+country_region,
+max(confirmed) as confirmed,
+max(recovered) as recovered,
+max(deaths) as deaths,
+(max(deaths)::float/max(confirmed)::float)::numeric(4,3) as lb,
+(max(deaths)::float/(max(recovered)::float+max(deaths)::float))::numeric(4,3) as ub
+from csse.dailies
+group by country_region
+having max(deaths)+max(recovered)>0
+order by max(confirmed) desc;
