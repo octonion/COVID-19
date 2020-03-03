b9ff2f71a8a2d5af646582b79139725fd3b1bd2d
Christopher D. Long <galizur@gmail.com>
2020-03-02 20:33:24 -0500

    Data update.

--- analytics/overall.sql
+++ analytics/overall.sql
@@ -26,11 +26,7 @@ sum(confirmed) as confirmed,
 sum(recovered) as recovered,
 sum(deaths) as deaths,
 (sum(deaths)::float/sum(confirmed)::float)::numeric(4,3) as lb,
-<<<<<<< HEAD
 (sum(deaths)::float/sum(least(2*recovered+deaths,confirmed))::float)::numeric(4,3) as mb,
-=======
-(sum(deaths)::float/(sum(2*recovered)::float+sum(deaths)::float))::numeric(4,3) as mb,
->>>>>>> 66619c7c086fbd07d930d9ac6e9f485aedfdb05f
 (sum(deaths)::float/(sum(recovered)::float+sum(deaths)::float))::numeric(4,3) as ub
 from data;
 
