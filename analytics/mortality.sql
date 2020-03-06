e288dc855b6c7070f35b1e825248a6ffd8c868e9
Christopher D. Long <galizur@gmail.com>
2020-03-06 01:58:00 -0500

    mb1 <> mb2

--- analytics/mortality.sql
+++ analytics/mortality.sql
@@ -28,11 +28,11 @@ country_region,
 sum(confirmed) as confirmed,
 sum(recovered) as recovered,
 sum(deaths) as deaths,
+--(sum(deaths)::float/sum(least(2*recovered+deaths,confirmed))::float)::numeric(4,3) as mb0,
 (sum(deaths)::float/sum(confirmed)::float)::numeric(4,3) as lb,
-(sum(deaths)::float/sum(least(2*recovered+deaths,confirmed))::float)::numeric(4,3) as mb0,
-(sum(deaths)::float/sum(1.9*(recovered+deaths))::float)::numeric(4,3) as mb1,
-(sum(deaths)::float/sum(1.9*confirmed)::float)::numeric(4,3) as mb2,
-(sum(deaths)::float/(sum(recovered)::float+sum(deaths)::float))::numeric(4,3) as ub
+(sum(deaths)::float/sum(1.9*confirmed)::float)::numeric(4,3) as mb1,
+(sum(deaths)::float/sum(1.9*(recovered+deaths))::float)::numeric(4,3) as mb2,
+(sum(deaths)::float/sum(recovered+deaths)::float)::numeric(4,3) as ub
 from data
 group by country_region
 having sum(deaths)+sum(recovered)>0
--- analytics/overall.sql
+++ analytics/overall.sql
@@ -27,11 +27,11 @@ select
 sum(confirmed) as confirmed,
 sum(recovered) as recovered,
 sum(deaths) as deaths,
+--(sum(deaths)::float/sum(least(2*recovered+deaths,confirmed))::float)::numeric(4,3) as mb0,
 (sum(deaths)::float/sum(confirmed)::float)::numeric(4,3) as lb,
-(sum(deaths)::float/sum(least(2*recovered+deaths,confirmed))::float)::numeric(4,3) as mb0,
-(sum(deaths)::float/sum(1.9*(recovered+deaths))::float)::numeric(4,3) as mb1,
-(sum(deaths)::float/sum(1.9*confirmed)::float)::numeric(4,3) as mb2,
-(sum(deaths)::float/(sum(recovered)::float+sum(deaths)::float))::numeric(4,3) as ub
+(sum(deaths)::float/sum(1.9*confirmed)::float)::numeric(4,3) as mb1,
+(sum(deaths)::float/sum(1.9*(recovered+deaths))::float)::numeric(4,3) as mb2,
+(sum(deaths)::float/sum(recovered+deaths)::float)::numeric(4,3) as ub
 from data
 ) to '/tmp/overall.csv' csv header;
 
